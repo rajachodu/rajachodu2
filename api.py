92b014cb25d7e4baabd7b09a0c46a1faed177e6a
rajachodu <motigand69@outlook.com>
2024-11-26 01:17:59 +0530

    Update api.py

--- api.py
+++ api.py
@@ -29,9 +29,9 @@ def configure_ngrok():
         print(f"Failed to configure ngrok: {str(e)}")
 
 def update_soul_txt(public_url):
-    with open("soul.txt", "w") as file:
+    with open("sikandar1.txt", "w") as file:
         file.write(public_url)
-    print(f"New ngrok link saved in soul.txt")
+    print(f"New ngrok link saved in sikandar.txt")
 
 def update_vps_soul_txt(public_url):
     vps_ip = "93.127.213.184"
@@ -43,13 +43,13 @@ def update_vps_soul_txt(public_url):
         ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
         ssh.connect(vps_ip, username=vps_user, password=vps_password)
         sftp = ssh.open_sftp()
-        with sftp.open("soul.txt", "w") as file:
+        with sftp.open("sikandar1.txt", "w") as file:
             file.write(public_url)
         sftp.close()
         ssh.close()
         print("Updated soul.txt on VPS successfully.")
     except Exception as e:
-        print(f"Failed to update soul.txt on VPS: {str(e)}")
+        print(f"Failed to update sikandar.txt on VPS: {str(e)}")
 
 def execute_command_async(command, duration):
     def run(command_id):
